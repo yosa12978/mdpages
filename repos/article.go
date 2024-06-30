472351b04ac7559530b0549da3fde125ec63cbcd
yosa12978 <yosa12978@gmail.com>
2024-06-30 16:39:42 +0300

    making repos

--- repos/article.go
+++ repos/article.go
@@ -30,7 +30,39 @@ func (a *articleRepo) Delete(ctx context.Context, id string) error {
 }
 
 func (a *articleRepo) GetAll(ctx context.Context) ([]types.Article, error) {
-	panic("unimplemented")
+	q := `
+		SELECT
+			a.id AS article_id, 
+			categ.id AS category_id, 
+			categ.name AS category_name,
+			comm.id AS commit_id, 
+			comm.title AS title, 
+			comm.created AS last_updated
+		FROM articles a 
+		INNER JOIN categories categ ON categ.id = a.category_id
+		INNER JOIN commits comm ON comm.id = (
+			SELECT id FROM commits WHERE article_id = a.id ORDER BY created DESC LIMIT 1
+		)  
+		ORDER BY title;
+	`
+	rows, err := a.db.QueryContext(ctx, q)
+	if err != nil {
+		return nil, err
+	}
+	articles := []types.Article{}
+	for rows.Next() {
+		article := types.Article{}
+		rows.Scan(
+			&article.Id,
+			&article.CategoryId,
+			&article.CategoryName,
+			&article.CommitId,
+			&article.Title,
+			&article.CommitCreated,
+		)
+		articles = append(articles, article)
+	}
+	return articles, nil
 }
 
 func (a *articleRepo) GetById(ctx context.Context, id string) (*types.Article, error) {
@@ -63,5 +95,9 @@ func (a *articleRepo) GetById(ctx context.Context, id string) (*types.Article, e
 }
 
 func (a *articleRepo) Update(ctx context.Context, id string, entity types.Article) error {
-	panic("unimplemented")
+	q := `
+		UPDATE articles SET category=$1 WHERE id=$2
+	`
+	_, err := a.db.ExecContext(ctx, q, entity.CategoryId, id)
+	return err
 }
